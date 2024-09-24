0ceec8bf79cf316612c1d7a0c4ea987d3d5d4a4d
전강현 <aniren@debian.moe>
2024-09-25 02:21:10 +0900

    Update exeMaker.bat

--- exeMaker.bat
+++ exeMaker.bat
@@ -1,6 +1,6 @@
 title exeMaker
 pyinstaller --onefile --uac-admin --icon=icon.ico osump3.py
+del /f /q osump3.spec osump3.exe
 copy dist\osump3.exe osump3.exe
-del /f /q osump3.spec
 rd /s /q __pycache__ && rd /s /q build && rd /s /q dist
 pause
